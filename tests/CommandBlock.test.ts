b48ad0e29e721ac4dd648ebfb5c3a157807d13a3
jjwall <jacob.wallace@utexas.edu>
2018-09-07 22:19:54 -0500

    Added node-canvas for future unit testing

--- tests/CommandBlock.test.ts
+++ tests/CommandBlock.test.ts
@@ -1,5 +1,8 @@
+/// <reference path="../src/declarations/node-canvas.d.ts" />
+
 import { CommandBlock } from "../src/CommandBlock";
 import { GameState } from "../src/main";
+import canvas from "canvas";
 
 test('constructs an instance of CommandBlock', () => {
     expect(new CommandBlock(new GameState(), 5, 5, 5, 5, "black"));
