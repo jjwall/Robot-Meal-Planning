fb00c19d131e66cd000073968c967c8542fb83ab
jjwall <jacob.wallace@utexas.edu>
2018-09-10 21:30:02 -0500

    Added jsdom for testing

---
+++ tests/GameState.test.ts
@@ -0,0 +1,27 @@
+/// <reference path="../src/declarations/node-canvas.d.ts" />
+
+// const jsdom = require('jsdom');
+// const { JSDOM } = jsdom;
+// const { createCanvas } = require('canvas');
+// // import { Canvas } from "canvas";
+// // const testCanvas = createCanvas(200, 200);
+// // const ctx = canvas.getContext('2d');
+// import { CommandBlock } from "../src/CommandBlock";
+// import { GameState } from "../src/main";
+// import { BaseBlock } from "../src/BaseBlock";
+// // import { IGameState } from "../src/IGameState";
+// // import canvas from "canvas";
+
+// test('constucts an instance of GameState', () => {
+    // const doc = (new JSDOM()).document;
+//     expect(new GameState({
+//         canvas: createCanvas(200, 200),
+//         ctx: createCanvas(200,200).getContext('2d'),
+//         rect: new ClientRect(),
+//         blocks: [],
+//         mouseX: 0,
+//         mouseY: 0,
+//         commandControl: true,
+//         flowControl: false
+//     })).toBeInstanceOf(GameState);
+// });
