5a7a343848cee5b4bfc687df7c60a90a2949213d
jjwall <jacob.wallace@utexas.edu>
2018-11-11 18:21:44 -0600

    Added coverage and GridBlock unit tests

--- tests/GridBlock.test.ts
+++ tests/GridBlock.test.ts
@@ -1,14 +1,26 @@
 import { GridBlock } from "../src/GridBlock";
 
-test('constucts an instance of GridBlock', () => {
-    expect(new GridBlock(
-        [],
-        10,
-        10,
-        10,
-        10,
-        10, 
-        10,
-        "color"
-    )).toBeInstanceOf(GridBlock);
+describe('GridBlock', () => {
+    let g: GridBlock;
+
+    beforeEach(() => {
+        g = new GridBlock([], 10, 10, 10, 10, 10, 10, "color");
+    });
+
+    it("constructs", () => {
+        expect(g).toBeInstanceOf(GridBlock);
+    });
+
+    it("draws to canvas", () => {
+        let c = document.createElement('canvas');
+        let ctx = c.getContext('2d');
+        g.draw(ctx);
+    });
+
+    it("updates", () => {
+        let copiedG = new GridBlock([], 10, 10, 10, 10, 10, 10, "color");
+        g.update();
+        // test state is the same since we update currently doesn't do anything
+        expect(copiedG).toEqual(g);
+    });
 });
